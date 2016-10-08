ab461c1e3d38082c9a75351e83c7ec8810fd685c
johnlee1 <telljlee@gmail.com>
2016-10-08 01:43:39 -0400

    add search feature

--- app.js
+++ app.js
@@ -4,32 +4,57 @@ let apiURL = 'apiURL';
 // set auth header for http requests
 Vue.http.headers.common['X-Auth-Token'] = 'X-Auth-Token';
 
-// contact item component
-Vue.component('contact-item', {
-  props: ['contact'],
-  template: '<li>{{ contact.first_name }}</li>'
-})
 
 new Vue({
 
     el: '#app',
 
     data: {
-        message: 'Contact Book',
-        contacts: ["contacts"]
+        fullContacts: ["fullContacts"],
+        searchContacts: ["searchContacts"],
+        showId: -1,
+        query: ''
     },
 
     created: function() {
         this.getContacts();
     },
 
+    watch: {
+        // whenever query changes, this function will run
+        query: function (newQuery) {
+            this.search(newQuery)
+        }
+    },
+
     methods: {
+
+        showContact: function(showId, contactId) {
+            if (showId == contactId) {
+                this.showId = -1;
+            } else {
+                this.showId = contactId;
+            }
+        },
 
         getContacts: function() {
             this.$http.get(apiURL).then((response) => {
-                this.$set(this.contacts, "contacts", response.body.data)
+                this.$set(this.searchContacts, "searchContacts", response.body.data);
+                this.$set(this.fullContacts, "fullContacts", response.body.data);
             });
-        }
+        },
+
+        search: function (query) {
+                contacts = [];
+                for (i=0; i<this.fullContacts[0].length;i++) {
+                    contact = JSON.stringify(this.fullContacts[0][i]).toLowerCase();
+                    if (contact.indexOf(query.toLowerCase()) > -1) {
+                        contacts.push(this.fullContacts[0][i])
+                    }
+                }
+                this.searchContacts = [contacts];
+            }
+        
     }
 
 })
